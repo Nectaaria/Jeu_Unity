811387e932e7513efa4726c6c273661ee7bff132
Sarah N <sarahniederits@gmail.com>
2024-12-09 17:53:38 +0100

    Added CameraManager and move

---
+++ Assets/scripts/CameraManager.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+public class CameraManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera camera;
+    public bool isLeftMoving = false;
+    public bool isRightMoving = false;
+    public bool isUpMoving = false;
+    public bool isDownMoving = false;
+    public void Update()
+    {
+        if (isLeftMoving)
+        {
+            camera.transform.position += new Vector3(-0.1f, 0, 0);
+        }
+
+        if (isRightMoving)
+        {
+            camera.transform.position += new Vector3(0.1f, 0, 0);
+        }
+
+        if (isUpMoving)
+        {
+            camera.transform.position += new Vector3(0, 0.1f, 0);
+        }
+
+        if (isDownMoving)
+        {
+            camera.transform.position += new Vector3(0, -0.1f, 0);
+        }
+    }
+    public void OnPointerExit()
+    {
+        if (gameObject.tag == "Left")
+        {
+            isLeftMoving = false;
+            
+        }
+        if (gameObject.tag == "Right")
+        {
+            isRightMoving = false;
+            
+        }
+        if (gameObject.tag == "Footer")
+        {
+            isDownMoving = false;
+            
+        }
+        if (gameObject.tag == "Header")
+        {
+            isUpMoving = false;
+            
+        }
+    }
+
+    public void OnPointerEnter()
+    {
+        if (gameObject.tag == "Left")
+        {
+            isLeftMoving = true;
+            
+        }
+        if (gameObject.tag == "Right")
+        {
+            isRightMoving = true;
+            
+        }
+        if (gameObject.tag == "Footer")
+        {
+            isDownMoving = true;
+            
+        }
+        if (gameObject.tag == "Header")
+        {
+            isUpMoving = true;
+            
+        }
+    }
+}
